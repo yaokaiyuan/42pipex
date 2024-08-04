e0bc9e64f4c651b20d4566ceb1a056df80b42294
Kyle Yao <iamkiger38@gmail.com>
2024-08-04 23:38:15 +0800

    bp

--- pipex.c
+++ pipex.c
@@ -16,12 +16,14 @@ static void	first_cmd(char **av, char **envp, int *fd)
 {
 	int	filein;
 
-	filein = open(av[1], O_RDONLY, 0777);
+	filein = open(av[1], O_RDONLY);
 	if (filein == -1)
 		error();
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(filein, STDIN_FILENO);
+	close(filein);
 	close(fd[0]);
+	close(fd[1]);
 	execute(av[2], envp);
 }
 
@@ -29,11 +31,13 @@ static void	second_cmd(char **av, char **envp, int *fd)
 {
 	int	fileout;
 
-	fileout = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	fileout = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (fileout == -1)
 		error();
 	dup2(fd[0], STDIN_FILENO);
 	dup2(fileout, STDOUT_FILENO);
+	close(fileout);
+	close(fd[0]);
 	close(fd[1]);
 	execute(av[3], envp);
 }
@@ -42,6 +46,7 @@ int	main(int ac, char **av, char **envp)
 {
 	int		fd[2];
 	pid_t	pid1;
+	pid_t	pid2;
 
 	if (ac == 5)
 	{
@@ -52,8 +57,15 @@ int	main(int ac, char **av, char **envp)
 			error();
 		if (pid1 == 0)
 			first_cmd(av, envp, fd);
+		pid2 = fork();
+		if (pid2 == -1)
+			error();
+		if (pid2 == 0)
+			second_cmd(av, envp, fd);
+		close(fd[0]);
+    	close(fd[1]);
 		waitpid(pid1, NULL, 0);
-		second_cmd(av, envp, fd);
+		waitpid(pid2, NULL, 0);
 	}
 	else
 	{
